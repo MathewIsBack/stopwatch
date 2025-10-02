d4e71ffb264a7b944e401d1f224101f8a511d03b
MathewIsBack <oseghaleokononfua@gmail.com>
2025-10-02 10:47:10 +0100

    skeleton for stopwatch

---
+++ stopwatch.py
@@ -0,0 +1,58 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+class Stopwatch(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time = QTime(0, 0, 0, 0)
+        self.time_label = QLabel("00:00:00.00", self)
+        self.start_button = QPushButton("start", self)
+        self.stop_button = QPushButton("stop", self)
+        self.reset_button = QPushButton("reset", self)
+        self.timer = QTimer(self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Stopwatch")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        vbox.addWidget(self.start_button)
+        vbox.addWidget(self.stop_button)
+        vbox.addWidget(self.reset_button)
+
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+
+        hbox = QHBoxLayout()
+
+        hbox.addWidget(self.start_button)
+        hbox.addWidget(self.stop_button)
+        hbox.addWidget(self.reset_button)
+
+        vbox.addLayout(hbox)
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def format_time(self, time):
+        pass
+
+    def update_display(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    stopwatch = Stopwatch()
+    stopwatch.show()
+    sys.exit(app.exec_())
+
